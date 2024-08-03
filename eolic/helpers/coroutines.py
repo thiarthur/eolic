ae506519c5e27e4df3732ae35c233a1107dad249
Thiago Goslar <thiarthur1@gmail.com>
2024-08-02 23:13:04 -0300

    feat: adds task gathering for pending tasks before exit

--- eolic/helpers/coroutines.py
+++ eolic/helpers/coroutines.py
@@ -14,8 +14,11 @@ def run_coroutine(coroutine: Callable[..., Any], *args, **kwargs) -> Any:
     Returns:
         Any: The result of the coroutine.
     """
-    loop = asyncio.get_event_loop()
-    if loop.is_running():
-        return asyncio.ensure_future(coroutine(*args, **kwargs))
-    else:
-        return loop.run_until_complete(coroutine(*args, **kwargs))
+    try:
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            return asyncio.ensure_future(coroutine(*args, **kwargs))
+        else:
+            return loop.run_until_complete(coroutine(*args, **kwargs))
+    except RuntimeError:
+        ...
--- eolic/task_manager.py
+++ eolic/task_manager.py
@@ -1,12 +1,32 @@
-"""Module for handling taslk in Eolic."""
+"""Module for handling tasks in Eolic."""
 
 import asyncio
+import atexit
+import signal
 from typing import Any, Callable
 from eolic.helpers.coroutines import run_coroutine
 
 
 class TaskManager:
     """Handles the creation and management of asynchronous tasks."""
+
+    def __init__(self) -> None:
+        """Initialize the TaskManager."""
+        self._register_cleanup()
+
+    def _register_cleanup(self) -> None:
+        """Register cleanup functions for the task manager to run before exiting the application."""
+        atexit.register(self._cleanup)
+        signal.signal(signal.SIGINT, self._handle_signal)
+        signal.signal(signal.SIGTERM, self._handle_signal)
+
+    def _cleanup(self):
+        """Clean up the task manager."""
+        self.wait_for_all()
+
+    def _handle_signal(self, *_) -> None:
+        """Handle termination signals."""
+        self._cleanup()
 
     def create_task(self, func: Callable[..., Any], *args, **kwargs) -> None:
         """Create a new asynchronous task."""
